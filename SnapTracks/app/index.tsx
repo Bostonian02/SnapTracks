01bb9051ba5e0f512a1d8323455db7d913756422
Bostonian02 <bostonian02@gmail.com>
2024-10-05 16:06:09 -0400

    Fix formatting on buttons on home page

--- SnapTracks/app/index.tsx
+++ SnapTracks/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { Link } from "expo-router";
 
@@ -8,23 +8,43 @@ export default function Index() {
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: 'center'
       }}
     >
       <ThemedText type="subtitle">Ready to make some noise?</ThemedText>
-      <Link href="/camera" asChild>
-        <Pressable>
-          <ThemedText>Start</ThemedText>
-        </Pressable>
-      </Link>
-      
-      {/* Temporary Button to navigate to NowPlaying */}
-      <Link href="/nowplaying" asChild>
-        <Pressable>
-          <ThemedText>Go to Now Playing</ThemedText>
-        </Pressable>
-      </Link>
+      <View style={styles.buttonContainer}>
+        <Link href="/camera" asChild>
+          <Pressable style={styles.button}>
+            <ThemedText>Start</ThemedText>
+          </Pressable>
+        </Link>
+        
+        {/* Temporary Button to navigate to NowPlaying */}
+        <Link href="/nowplaying" asChild>
+          <Pressable style={styles.button}>
+            <ThemedText>Go to Now Playing</ThemedText>
+          </Pressable>
+        </Link>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: '80%',
+    marginVertical: 15,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    backgroundColor: '#007AFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
