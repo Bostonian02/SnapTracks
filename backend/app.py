7d18629ec4211f00f91b0a4a0295ce85b5cbe7ad
Bostonian02 <bostonian02@gmail.com>
2024-10-05 22:18:45 -0400

    Update port to 5001

--- backend/app.py
+++ backend/app.py
@@ -359,5 +359,5 @@ def get_image_description(image_bytes):
 
 if __name__ == "__main__":
     # Run the Flask app
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
